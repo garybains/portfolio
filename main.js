0c1194191e3909c059623c65eb4d833afbd98b0a
Gary Bains <gsb.gurpreet@gmail.com>
2017-04-08 16:52:33 -0400

    Get the modal working

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 console.log("JS is working");
 
+$(document).ready(function() {
+    // Init Modal
+    $('.modal').modal();
+});
+
+$("#tic_img").on("click", function() {
+    console.log("JS is reponding for img Click");
+});
