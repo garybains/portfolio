5a276c4f025016b282d00208f5ce173d7d27259c
Gary Bains <gsb.gurpreet@gmail.com>
2017-04-08 21:15:44 -0400

    Add capability to dynamically change modal image based on which project img was clicked

--- main.js
+++ main.js
@@ -5,6 +5,23 @@ $(document).ready(function() {
     $('.modal').modal();
 });
 
-$("#tic_img").on("click", function() {
-    console.log("JS is reponding for img Click");
-});
+$("#tic_img").click( function() {imgClicked("tic")} );
+$("#res_img").click( function() {imgClicked("res")} );
+
+var images = {
+    "tic": ["tic_1.png", "tic_2.png"]
+}
+
+function imgClicked(imgName) {
+
+    var modal_img = $("#modal_img");
+
+    if (imgName === "tic") {
+        console.log("Tic image clicked");
+        modal_img.attr('src', 'img/' + images["tic"][0] );
+    } else {
+        console.log("Some other image clicked");
+        modal_img.attr('src', 'img/' + images["tic"][1] );
+    }
+
+}
