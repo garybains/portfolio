618a60b35ea8576dc6a53edcda71359b77d7847c
Gary Bains <gsb.gurpreet@gmail.com>
2017-04-09 12:19:23 -0400

    Add capability to dynamically change modal image based on which project img was clicked

--- main.js
+++ main.js
@@ -7,21 +7,44 @@ $(document).ready(function() {
 
 $("#tic_img").click( function() {imgClicked("tic")} );
 $("#res_img").click( function() {imgClicked("res")} );
-
-var images = {
-    "tic": ["tic_1.png", "tic_2.png"]
-}
+$("#buseeta_img").click( function() {imgClicked("buseeta")} );
+$("#car_portal_img").click( function() {imgClicked("car_portal")} );
+$("#selfie_img").click( function() {imgClicked("selfie")} );
+$("#quiz_img").click( function() {imgClicked("quiz")} );
 
 function imgClicked(imgName) {
 
+    var images = {
+        "tic": ["tic_1.png", "tic_2.png"],
+        "res": ["res_vis_1.png", "res_vis_2.png"],
+        "buseeta": ["buseeta_1.png"],
+        "car_portal": ["car_portal_1.png"],
+        "selfie": [],
+        "quiz": ["quiz_1.png"]
+
+    }
     var modal_img = $("#modal_img");
 
     if (imgName === "tic") {
-        console.log("Tic image clicked");
         modal_img.attr('src', 'img/' + images["tic"][0] );
-    } else {
-        console.log("Some other image clicked");
-        modal_img.attr('src', 'img/' + images["tic"][1] );
+
+    } else if (imgName === "res") {
+        modal_img.attr('src', 'img/' + images["res"][0] );
+
+    } else if (imgName === "buseeta") {
+        modal_img.attr('src', 'img/' + images["buseeta"][0] );
+    }
+     else if (imgName === "car_portal") {
+        modal_img.attr('src', 'img/' + images["car_portal"][0] );
+    }
+    else if (imgName === "selfie") {
+        modal_img.attr('src', 'img/' + images["selfie"][0] );
+    }
+    else if (imgName === "quiz") {
+        modal_img.attr('src', 'img/' + images["quiz"][0] );
+    }
+    else {
+        console.log("The universe is inherintly inconsistent");
     }
 
 }
